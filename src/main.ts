02867f8893a52add4406d61e9898842af5864482
Ahmed-ES-SH <ahmedshehata9889@gmail.com>
2026-04-06 06:35:01 +0200

    ready for vercel- v2

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,67 @@
+// import { NestFactory } from '@nestjs/core';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { AppModule } from './app.module';
+// import helmet from 'helmet';
+// import { ValidationPipe } from '@nestjs/common';
+// import cookieParser from 'cookie-parser';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.use(cookieParser());
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//     }),
+//   );
+
+//   app.use(helmet());
+
+//   app.enableCors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   });
+
+//   const config = new DocumentBuilder()
+//     .setTitle('Cats example')
+//     .setDescription('The cats API description')
+//     .setVersion('1.0')
+//     .addTag('cats')
+//     .build();
+//   const documentFactory = () => SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, documentFactory);
+
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const server = express();
+
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
 
   app.use(cookieParser());
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-
   app.use(helmet());
-
   app.enableCors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
@@ -30,9 +73,14 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('cats')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.init();
+};
+
+export default async (req: any, res: any) => {
+  await createNestServer(server);
+  server(req, res);
+};
